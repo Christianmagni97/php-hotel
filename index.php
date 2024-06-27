6aca0a3267b7f98b666c600adae5bf86c378f963
Christianmagni97 <christianmagni28@gmail.com>
2024-06-28 00:47:46 +0200

    milestone2

--- index.php
+++ index.php
@@ -50,6 +50,40 @@
     <title>PHP hotelt</title>
 </head>
 <body>
-    
+    <h1 class='d-flex justify-content-center text-primary'>I nostri hotel:</h1>
+    <main>
+        <section>
+            <table class="table m-2">
+                <thead>
+                    <tr>
+                        <th class ='text-primary'scope="col">Name</th>
+                        <th class ='text-primary'scope="col">Description</th>
+                        <th class ='text-primary'scope="col">Parking</th>
+                        <th class ='text-primary'scope="col">Vote</th>
+                        <th class ='text-primary'scope="col">Distance to center</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($hotels as $hotel ) {?>
+                    <tr>
+                        <th scope="row"><?php echo $hotel['name']  ?></th>
+                        <td><?php echo $hotel['description']  ?></td>
+                        <td><?php echo $hotel['parking']  ?></td>
+                        <td><?php echo $hotel['vote']  ?></td>
+                        <td><?php echo $hotel['distance_to_center']  ?></td>
+                    </tr>
+                    <?php }?>
+                </tbody>
+            </table>
+        </section>
+        <form action='./index.php' action='GET'>
+            <div class="mb-3 form-check">
+                <input type="checkbox"  name="parking" id="parking" />
+                <label id="parking">Incluso parcheggio</label>
+                <button type="submit" class="btn btn-primary m-2">Send</button>
+            </div>
+        </form>
+    </main>
+
 </body>
 </html>
